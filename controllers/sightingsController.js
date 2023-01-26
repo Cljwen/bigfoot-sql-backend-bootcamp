e469299a7dc07d787fb762c368396885cff80b55
Cljwen <carmenlaujwn@gmail.com>
2023-01-27 01:19:56 +0800

    version1-error

--- controllers/sightingsController.js
+++ controllers/sightingsController.js
@@ -1,8 +1,9 @@
 const BaseController = require("./baseController");
 
 class SightingsController extends BaseController {
-  constructor(model) {
+  constructor(model, commentModel) {
     super(model);
+    this.commentModel = commentModel;
   }
 
   // Retrieve specific sighting
@@ -11,6 +12,46 @@ class SightingsController extends BaseController {
     try {
       const sighting = await this.model.findByPk(sightingId);
       return res.json(sighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  // create new sighting
+  async createSighting(req, res) {
+    try {
+      const newSighting = await this.model.create(req.body);
+      return res.json(newSighting);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  // Get all comments on sighting
+  async getComments(req, res) {
+    const { sightingId } = req.params;
+    try {
+      const comments = await this.commentModel.findAll({
+        where: {
+          sighting_id: sightingId,
+        },
+      });
+      return res.json(comments);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  // create new comment
+  async createComment(req, res) {
+    const { sightingId } = req.params;
+    const { content } = req.body;
+    try {
+      const newComment = await this.commentModel.create({
+        content: content,
+        sighting_id: sightingId,
+      });
+      return res.json(newComment);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
--- routers/sightingsRouter.js
+++ routers/sightingsRouter.js
@@ -9,6 +9,17 @@ class SightingsRouter {
     // we will insert routes into here later on
     router.get("/", this.controller.getAll.bind(this.controller));
     router.get("/:sightingId", this.controller.getOne.bind(this.controller));
+    router.post("/", this.controller.createSighting.bind(this.controller));
+
+    router.get(
+      "/:sightingId/comments",
+      this.controller.getComments.bind(this.controller)
+    );
+    router.post(
+      "/:sightingId/comments",
+      this.controller.createComment.bind(this.controller)
+    );
+
     return router;
   }
 }
